c609b0d2b7f4eb062d5acdebbb999a16364adcd2
PC <amolujagare@gmail.com>
2021-02-04 08:34:23 +0530

    Initial commit

---
+++ src/Operators.java
@@ -0,0 +1,11 @@
+public class Operators {
+
+    public static void main(String[] args) {
+
+        int a = 50;
+
+        System.out.println( a >= 50 );
+
+        System.out.println(50 > 50 );
+    }
+}
